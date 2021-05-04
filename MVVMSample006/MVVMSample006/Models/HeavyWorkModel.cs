3098215c4be002dfd186b399b2f5a1ee1b948564
emu2021makuake <81235941+emu2021makuake@users.noreply.github.com>
2021-05-04 15:17:48 +0900

    Add files via upload

---
+++ MVVMSample006/MVVMSample006/Models/HeavyWorkModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MVVMSample006.Models
+{
+    public class HeavyWorkModel
+    {
+        public async Task HeavyWorkAsync(
+            IProgress<ProgressInfo> progress,
+            CancellationToken token)
+        {
+            try
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    token.ThrowIfCancellationRequested();
+
+                    await Task.Delay(50);
+
+                    progress.Report(new ProgressInfo(i, $"{i + 1} cases processed."));
+                }
+
+                progress.Report(new ProgressInfo(0, "***** Done *****"));
+            }
+            catch (OperationCanceledException)
+            {
+                progress.Report(new ProgressInfo(0, "***** Cancelled *****"));
+                return;
+            }
+
+
+        }
+    }
+}
